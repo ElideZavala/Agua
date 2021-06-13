3aa8a4f8f0b3318f5df25b273cf1625f47444398
ElideZavala <zavalavinagreelide@gmail.com>
2021-06-12 18:51:01 -0600

    efectos de llenado y operaciónes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const smallCups = document.querySelectorAll('.cup-small');
-const liters = document.getElementById('liters');
+const listers = document.getElementById('listers');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
@@ -31,7 +31,19 @@ function updateBigCUP() {
 	const totalCups = smallCups.length;
 
 	if (fullCups === 0) {
-		porcentage.style.visibility = 'hidden'
+		percentage.style.visibility = 'hidden';
+		percentage.style.height = 0;
+	} else {
+		percentage.style.visibility = 'visible';
+		percentage.style.height = `${fullCups / totalCups * 330}px`
+		percentage.innerText = `${fullCups / totalCups * 100}%`
 	}
 
+	if (fullCups === totalCups) {
+		remained.style.visibility = 'hidden';
+		remained.style.height = 0;
+	} else {
+		remained.style.visibility = 'visible';
+		listers.innerText = `${2 -(250 * fullCups / 1000) }L`;
+	}
 }
